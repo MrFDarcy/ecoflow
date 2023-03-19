9924ad281680235684190d18b67c7b75069c87cc
Aditya Patil <patiladityadr@gmail.com>
2023-03-19 14:25:30 +0530

    post upload frontend created

--- lib/screens/post_upload_screen.dart
+++ lib/screens/post_upload_screen.dart
@@ -1,7 +1,30 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class PostUploadScreen extends StatelessWidget {
-  const PostUploadScreen({super.key});
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class PostUploadScreen extends StatefulWidget {
+  const PostUploadScreen({Key? key}) : super(key: key);
+
+  @override
+  _PostUploadScreenState createState() => _PostUploadScreenState();
+}
+
+class _PostUploadScreenState extends State<PostUploadScreen> {
+  File? _imageFile;
+
+  Future<void> _pickImage(ImageSource source) async {
+    final pickedFile = await ImagePicker().pickImage(source: source);
+    setState(() {
+      _imageFile = File(pickedFile!.path);
+    });
+  }
+
+  void _clear() {
+    setState(() {
+      _imageFile = null;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +32,119 @@ class PostUploadScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('PostUploadScreen'),
       ),
-      body: const Center(
-        child: Text('PostUploadScreen'),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 50),
+              GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return SafeArea(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            ListTile(
+                              leading: const Icon(Icons.camera_alt),
+                              title: const Text('Take a picture'),
+                              onTap: () {
+                                Navigator.pop(context);
+                                _pickImage(ImageSource.camera);
+                              },
+                            ),
+                            ListTile(
+                              leading: const Icon(Icons.photo_library),
+                              title: const Text('Choose from gallery'),
+                              onTap: () {
+                                Navigator.pop(context);
+                                _pickImage(ImageSource.gallery);
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: Container(
+                  // clipBehavior: Clip.antiAliasWithSaveLayer,
+                  width: 300,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    // border: Border.all(
+                    //   color: Colors.grey[400]!,
+                    //   width: 2.0,
+                    // ),
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: _imageFile != null
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(
+                            File(_imageFile!.path),
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                      : const Icon(
+                          Icons.camera_alt,
+                          size: 100,
+                        ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: const [
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter a caption',
+                        border: OutlineInputBorder(),
+                      ),
+                      maxLength: 100,
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Enter description',
+                        border: OutlineInputBorder(),
+                      ),
+                      maxLines: null,
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              ),
+
+              // submit button
+
+              const SizedBox(height: 50),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.green,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Submit'),
+                ),
+              ),
+
+              const SizedBox(height: 50),
+            ],
+          ),
+        ),
       ),
     );
   }
